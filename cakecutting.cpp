713f3178cc50e05d29d20c3736db4a527f5eb528
Noah Hessen Bjerke <noahhb13@gmail.com>
2026-02-03 17:03:18 +0100

    Cake Cutting

---
+++ cakecutting.cpp
@@ -0,0 +1,88 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+template<class P> bool onSegment(P s, P e, P p) {
+	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
+}
+
+template<class P> vector<P> segInter(P a, P b, P c, P d) {
+	auto oa = c.cross(d, a), ob = c.cross(d, b),
+	     oc = a.cross(b, c), od = a.cross(b, d);
+	// Checks if intersection is single non-endpoint point.
+	if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
+		return {(a * ob - b * oa) / (ob - oa)};
+	set<P> s;
+	if (onSegment(c, d, a)) s.insert(a);
+	if (onSegment(c, d, b)) s.insert(b);
+	if (onSegment(a, b, c)) s.insert(c);
+	if (onSegment(a, b, d)) s.insert(d);
+	return {all(s)};
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+
+    int n;cin>>n;
+    vector<pair<Point<long long>,Point<long long>>> lines;
+    rep(i,0,n){
+        long long a,b,c,d;cin>>a>>b>>c>>d;
+        Point<ll> p1(a,b), p2(c,d);
+        lines.push_back({p1,p2});
+    }
+
+    ll sm = 1;
+    rep(i,0,n){
+        sm++;
+        set<Point<ll>> s;
+        s.insert(lines[i].first);s.insert(lines[i].second);
+        rep(j,0,i){
+            s.insert(lines[j].first);s.insert(lines[j].second);
+            auto a = segInter(lines[i].first,lines[i].second,lines[j].first,lines[j].second);
+            if(!a.empty()){
+                Point<ll> p = a[0];
+                if(s.find(p) == s.end()){
+                    sm++;
+                }
+                s.insert(p);
+            }
+        }
+    }
+    cout << sm << "\n";
+}
